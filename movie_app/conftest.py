4c613c4177faad656da3a7c87c32f52acfaca2aa
cooklee <sbbogus@gmail.com>
2023-07-27 16:24:24 +0200

    calosc

--- movie_app/conftest.py
+++ movie_app/conftest.py
@@ -1,6 +1,8 @@
 import pytest
+from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
 
-from movie_app.models import Genre, Person, Producer
+from movie_app.models import Genre, Person, Producer, Movie
 
 
 @pytest.fixture
@@ -24,4 +26,31 @@ def producers():
     lst = []
     for x in range(5):
         lst.append(Producer.objects.create(name=x, city=x, year=x))
+    return lst
+
+
+@pytest.fixture
+def user():
+    u = User.objects.create(username='testowy')
+    return u
+@pytest.fixture
+def user_with_permission(user):
+    content_type = ContentType.objects.get(app_label='movie_app',
+                                           model='genre')
+    permissions = Permission.objects.filter(content_type=content_type)
+    user.user_permissions.set(permissions)
+    return user
+
+@pytest.fixture
+def movies(producers, persons, genres):
+    lst = []
+    for x in range(5):
+        m = Movie()
+        m.title = x
+        m.year = x
+        m.director = persons[x]
+        m.producer = producers[x]
+        m.save()
+        m.genre.set(genres)
+        lst.append(m)
     return lst
--- movie_app/tests.py
+++ movie_app/tests.py
@@ -2,7 +2,8 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
-from movie_app.forms import SearchPersonForm
+from movie_app.forms import SearchPersonForm, AddPersonForm, AddMovieModelForm
+from movie_app.models import Person, Producer, Movie, Comment
 
 
 @pytest.mark.django_db
@@ -40,9 +41,9 @@ def test_person_list(persons):
 def test_person_search(persons):
     browser = Client()
     url = reverse('list_person')
-    data = {'first_name':'1',
-            'last_name':'1',
-            'year':1}
+    data = {'first_name': '1',
+            'last_name': '1',
+            'year': 1}
     response = browser.get(url, data)
     assert response.status_code == 200
     assert response.context['persons'].get(**data)
@@ -61,10 +62,133 @@ def test_producer_list_view(producers):
 def test_producer_detail_view(producers):
     browser = Client()
     p = producers[0]
-    url = reverse('detail_producer', kwargs={'id':p.id})
+    url = reverse('detail_producer', kwargs={'id': p.id})
     response = browser.get(url)
     assert response.status_code == 200
     assert response.context['producer'] == p
 
 
+@pytest.mark.django_db
+def test_detail_genre_view_not_login(genres):
+    browser = Client()
+    g = genres[0]
+    url = reverse('detail_genre', args=(g.id,))
+    # browser.force_login(user)
+    response = browser.get(url)
+    assert response.status_code == 302
 
+
+@pytest.mark.django_db
+def test_detail_genre_view_login_without_permission(genres, user):
+    browser = Client()
+    g = genres[0]
+    url = reverse('detail_genre', args=(g.id,))
+    browser.force_login(user)
+    response = browser.get(url)
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_detail_genre_view_login_with_permission(genres, user_with_permission):
+    browser = Client()
+    g = genres[0]
+    url = reverse('detail_genre', args=(g.id,))
+    browser.force_login(user_with_permission)
+    response = browser.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_movie_list_view(movies):
+    browser = Client()
+    url = reverse('list_movie')
+    response = browser.get(url)
+    assert response.status_code == 200
+    assert list(response.context['movies']) == movies
+
+
+@pytest.mark.djnago_db
+def test_add_person_get():
+    browser = Client()
+    url = reverse('add_person')
+    response = browser.get(url)
+    assert response.status_code == 200
+    assert isinstance(response.context['form'], AddPersonForm)
+
+
+@pytest.mark.django_db
+def test_add_person_post():
+    browser = Client()
+    url = reverse('add_person')
+    data = {
+        'first_name': 'sla',
+        'last_name': 'bo',
+        'year': 1293
+    }
+    response = browser.post(url, data)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('add_person'))
+    assert Person.objects.get(**data)
+
+
+@pytest.mark.django_db
+def test_add_producer_get():
+    browser = Client()
+    url = reverse('add_producer')
+    response = browser.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_producer_post():
+    browser = Client()
+    url = reverse('add_producer')
+    data = {
+        'name': 'Py',
+        'city': 'Ton',
+        'year': 1921
+    }
+    response = browser.post(url, data)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('add_producer'))
+    assert Producer.objects.get(**data)
+
+
+@pytest.mark.django_db
+def test_get_add_movie():
+    browser = Client()
+    url = reverse('add_movie')
+    response = browser.get(url)
+    assert response.status_code == 200
+    assert isinstance(response.context['form'], AddMovieModelForm)
+
+@pytest.mark.django_db
+def test_post_add_movie(persons, producers, genres):
+    browser = Client()
+    url = reverse('add_movie')
+    data = {
+        'title':'ptysi mysie',
+        'year': 1999,
+        'director':persons[0].id,
+        'producer':producers[0].id,
+        'genre':[x.id for x in genres]
+    }
+    response = browser.post(url, data)
+    assert response.status_code == 302
+    del data['genre']
+    obj = Movie.objects.get(**data)
+    assert list(obj.genre.all()) == genres
+
+
+@pytest.mark.django_db
+def test_add_comment_to_movie(movies, user):
+    movie = movies[0]
+    browser = Client()
+    url = reverse('add_comment', args=(movie.id, ))
+    browser.force_login(user)
+    data = {
+        'text':'ala ma kota'
+    }
+    response = browser.post(url, data)
+    assert response.status_code == 302
+    assert Comment.objects.get(**data)
